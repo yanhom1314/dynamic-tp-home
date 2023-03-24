4b9477fa73fac2df7ea452eb4c061427fff5b059
yanhom <1772140053@qq.com>
2023-03-24 10:30:18 +0800

    Delete docs/.vuepress/.temp directory

--- docs/.vuepress/.temp/md-enhance/config.js
+++
@@ -1,8 +0,0 @@
-import { defineClientConfig } from "@vuepress/client";
-import "/Users/yanhom/opensource/dynamic-tp-home/node_modules/vuepress-plugin-md-enhance/lib/client/styles/container/index.scss";
-
-export default defineClientConfig({
-  enhance: ({ app }) => {
-
-  },
-});
