400dd6e7ed0500a80626a1bfa14b6ed4b63fb15a
yanhom <yanhom1314@foxmail.com>
2024-12-17 23:38:52 +0800

    deploy

---
+++ baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?271bc4a4c9eba8c190a787fa6145291a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
